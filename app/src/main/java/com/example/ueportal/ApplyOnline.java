b7f8aa9e52fff13e067f8b4c4ed3169b603e0946
Usman <usmangq12@gmail.com>
2019-08-01 00:30:59 +0500

    comit change adding webview online apply

--- app/src/main/java/com/example/ueportal/ApplyOnline.java
+++ app/src/main/java/com/example/ueportal/ApplyOnline.java
@@ -2,15 +2,29 @@ package com.example.ueportal;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class ApplyOnline extends AppCompatActivity {
+    private WebView webView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_apply_online);
 
+        webView = findViewById(R.id.webView1);
+        webView.setWebViewClient(new WebViewClient());
+        webView.loadUrl("http://web.ue.edu.pk/apply/");
 
+    }
 
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
     }
 }
